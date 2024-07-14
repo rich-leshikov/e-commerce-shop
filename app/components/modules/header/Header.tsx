57e2cd111306d6e9732c3c85936aef971a8df9a7
Richard Leshik <richardleshik@gmail.com>
2024-07-14 18:04:22 +0300

    feat: add Header component

---
+++ app/components/modules/header/Header.tsx
@@ -0,0 +1,15 @@
+import { useLang } from '@/hooks'
+
+export const Header = () => {
+  const { lang, translations } = useLang()
+
+  return (
+    <header className='header'>
+      <div className='container header__container'>
+        <button className='btn-reset header__burger'>
+          {translations[lang].header.menu_btn}
+        </button>
+      </div>
+    </header>
+  )
+}
