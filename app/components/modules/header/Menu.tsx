88283d9587857d20a751d8af3d0e1bc0287bb6b5
Richard Leshik <richardleshik@gmail.com>
2024-07-18 23:30:26 +0300

    feat: update Menu component

--- app/components/modules/header/Menu.tsx
+++ app/components/modules/header/Menu.tsx
@@ -1,11 +1,13 @@
 'use client'
 import { useState } from 'react'
 import { useUnit } from 'effector-react'
+import { usePathname } from 'next/navigation'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useLang } from '@/hooks'
 import { AllowedLangs } from '@/constants'
 import { $menuIsOpen, closeMenu, setLang } from '@/context'
 import { removeOverflowHiddenFromBody } from '@/lib'
-import { Logo } from '@/components'
+import { Logo, Accordion, MenuLinkItem } from '@/components'
 
 export const Menu = () => {
   const [showCatalogList, setShowCatalogList] = useState(false)
@@ -13,6 +15,7 @@ export const Menu = () => {
   const [showContactsList, setShowContactsList] = useState(false)
   const menuIsOpen = useUnit($menuIsOpen)
   const { lang, translations } = useLang()
+  const pathname = usePathname()
 
   const handleSwitchLang = (lang: string) => {
     setLang(lang as AllowedLangs)
@@ -22,39 +25,207 @@ export const Menu = () => {
   const handleSwitchLangToRu = () => handleSwitchLang('ru')
   const handleSwitchLangToEn = () => handleSwitchLang('en')
 
+  const handleShowCatalogList = () => {
+    setShowCatalogList(true)
+    setShowBuyersList(false)
+    setShowContactsList(false)
+  }
+
   const handleCloseMenu = () => {
     removeOverflowHiddenFromBody()
     closeMenu()
   }
 
+  const handleRedirectToCatalog = (path: string) => {
+    if (pathname.includes('/catalog')) {
+      window.history.pushState({ path }, '', path)
+      window.location.reload()
+    }
+
+    handleCloseMenu()
+  }
+
+  const clothLinks = [
+    {
+      id: 1,
+      text: translations[lang].comparison['t-shirts'],
+      href: '/catalog/cloth?offset=0&type=t-shirts',
+    },
+    {
+      id: 2,
+      text: translations[lang].comparison['long-sleeves'],
+      href: '/catalog/cloth?offset=0&type=long-sleeves',
+    },
+    {
+      id: 3,
+      text: translations[lang].comparison.hoodie,
+      href: '/catalog/cloth?offset=0&type=hoodie',
+    },
+    {
+      id: 4,
+      text: translations[lang].comparison.outerwear,
+      href: '/catalog/cloth?offset=0&type=outerwear',
+    },
+  ]
+
+  const accessoriesLinks = [
+    {
+      id: 1,
+      text: translations[lang].comparison.bags,
+      href: '/catalog/accessories?offset=0&type=bags',
+    },
+    {
+      id: 2,
+      text: translations[lang].comparison.headdress,
+      href: '/catalog/accessories?offset=0&type=headdress',
+    },
+    {
+      id: 3,
+      text: translations[lang].comparison.umbrella,
+      href: '/catalog/accessories?offset=0&type=umbrella',
+    },
+  ]
+
+  const souvenirsLinks = [
+    {
+      id: 1,
+      text: translations[lang].comparison['business-souvenirs'],
+      href: '/catalog/souvenirs?offset=0&type=business-souvenirs',
+    },
+    {
+      id: 2,
+      text: translations[lang].comparison['promotional-souvenirs'],
+      href: '/catalog/souvenirs?offset=0&type=promotional-souvenirs',
+    },
+  ]
+
+  const officeLinks = [
+    {
+      id: 1,
+      text: translations[lang].comparison.notebook,
+      href: '/catalog/office?offset=0&type=notebook',
+    },
+    {
+      id: 2,
+      text: translations[lang].comparison.pen,
+      href: '/catalog/office?offset=0&type=pen',
+    },
+  ]
+
   return (
     <nav className={`nav-menu ${menuIsOpen ? 'open' : 'close'}`}>
-      <div className='container nav-menu__container' />
-      <div className={`nav-menu__logo ${menuIsOpen ? 'open' : 'close'}`}>
-        <Logo />
-      </div>
-      <img
-        src='/img/menu-bg.png'
-        alt='menu backgroud'
-        className={`nav-menu__bg ${menuIsOpen ? 'open' : ''}`}
-      />
-      <button
-        className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
-        onClick={handleCloseMenu}
-      />
-      <div className={`nav-menu__lang ${menuIsOpen ? 'open' : ''}`}>
+      <div className='container nav-menu__container'>
+        <div className={`nav-menu__logo ${menuIsOpen ? 'open' : ''}`}>
+          <Logo />
+        </div>
+        <img
+          src='/img/menu-bg.png'
+          alt='menu backgroud'
+          className={`nav-menu__bg ${menuIsOpen ? 'open' : ''}`}
+        />
         <button
-          className={`btn-reset nav-menu__lang__btn ${lang === 'ru' ? 'lang-active' : ''}`}
-          onClick={handleSwitchLangToRu}
-        >
-          RU
-        </button>
-        <button
-          className={`btn-reset nav-menu__lang__btn ${lang === 'en' ? 'lang-active' : ''}`}
-          onClick={handleSwitchLangToEn}
-        >
-          EN
-        </button>
+          className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
+          onClick={handleCloseMenu}
+        />
+        <div className={`nav-menu__lang ${menuIsOpen ? 'open' : ''}`}>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${lang === 'ru' ? 'lang-active' : ''}`}
+            onClick={handleSwitchLangToRu}
+          >
+            RU
+          </button>
+          <button
+            className={`btn-reset nav-menu__lang__btn ${lang === 'en' ? 'lang-active' : ''}`}
+            onClick={handleSwitchLangToEn}
+          >
+            EN
+          </button>
+        </div>
+        <ul className={`list-reset nav-menu__list ${menuIsOpen ? 'open' : ''}`}>
+          <li className='nav-menu__list__item'>
+            <button
+              className='btn-reset nav-menu__list__item__btn'
+              onMouseEnter={handleShowCatalogList}
+            >
+              {translations[lang].main_menu.catalog}
+            </button>
+            <AnimatePresence>
+              {showCatalogList && (
+                <motion.ul
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  exit={{ opacity: 0 }}
+                  className='list-reset nav-menu__accordion'
+                >
+                  <li className='nav-menu__accordion__item'>
+                    <Accordion
+                      title={translations[lang].main_menu.cloth}
+                      titleClass={'btn-reset nav-menu__accordion__item__title'}
+                    >
+                      <ul className='list-reset nav-menu__accordion__item__list'>
+                        {clothLinks.map((item) => (
+                          <MenuLinkItem
+                            key={item.id}
+                            item={item}
+                            handleRedirectToCatalog={handleRedirectToCatalog}
+                          />
+                        ))}
+                      </ul>
+                    </Accordion>
+                  </li>
+                  <li className='nav-menu__accordion__item'>
+                    <Accordion
+                      title={translations[lang].main_menu.accessories}
+                      titleClass={'btn-reset nav-menu__accordion__item__title'}
+                    >
+                      <ul className='list-reset nav-menu__accordion__item__list'>
+                        {accessoriesLinks.map((item) => (
+                          <MenuLinkItem
+                            key={item.id}
+                            item={item}
+                            handleRedirectToCatalog={handleRedirectToCatalog}
+                          />
+                        ))}
+                      </ul>
+                    </Accordion>
+                  </li>
+                  <li className='nav-menu__accordion__item'>
+                    <Accordion
+                      title={translations[lang].main_menu.souvenirs}
+                      titleClass={'btn-reset nav-menu__accordion__item__title'}
+                    >
+                      <ul className='list-reset nav-menu__accordion__item__list'>
+                        {souvenirsLinks.map((item) => (
+                          <MenuLinkItem
+                            key={item.id}
+                            item={item}
+                            handleRedirectToCatalog={handleRedirectToCatalog}
+                          />
+                        ))}
+                      </ul>
+                    </Accordion>
+                  </li>
+                  <li className='nav-menu__accordion__item'>
+                    <Accordion
+                      title={translations[lang].main_menu.office}
+                      titleClass={'btn-reset nav-menu__accordion__item__title'}
+                    >
+                      <ul className='list-reset nav-menu__accordion__item__list'>
+                        {officeLinks.map((item) => (
+                          <MenuLinkItem
+                            key={item.id}
+                            item={item}
+                            handleRedirectToCatalog={handleRedirectToCatalog}
+                          />
+                        ))}
+                      </ul>
+                    </Accordion>
+                  </li>
+                </motion.ul>
+              )}
+            </AnimatePresence>
+          </li>
+        </ul>
       </div>
     </nav>
   )
