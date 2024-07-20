ec4ccb1a82791d24ba0060d859b095f9ad1658cf
Richard Leshik <richardleshik@gmail.com>
2024-07-20 20:38:30 +0300

    feat: add util function getWindowWidth

--- app/lib/utils/common.ts
+++ app/lib/utils/common.ts
@@ -7,3 +7,10 @@ export const removeOverflowHiddenFromBody = () => {
   const body = document.querySelector('body') as HTMLBodyElement
   body.classList.remove('overflow-hidden')
 }
+
+export const getWindowWidth = () => {
+  const { innerWidth: windowWidth } =
+    typeof window !== 'undefined' ? window : { innerWidth: 0 }
+
+  return windowWidth
+}
