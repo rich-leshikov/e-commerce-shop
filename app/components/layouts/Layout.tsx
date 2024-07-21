c3d99c84f324e1e0704c2a5849477e65a740b516
Richard Leshik <richardleshik@gmail.com>
2024-07-21 21:04:38 +0300

    feat: update Layout.tsx

--- app/components/layouts/Layout.tsx
+++ app/components/layouts/Layout.tsx
@@ -1,16 +1,20 @@
+'use client'
 import { FC, ReactNode } from 'react'
-import { Header } from '@/components'
+import { Header, MobileNavbar } from '@/components'
+import { useMediaQuery } from '@/hooks'
 
 type Props = {
   children: ReactNode
 }
 
 export const Layout: FC<Props> = ({ children }) => {
-  const a = 1
+  const isMedia800 = useMediaQuery(800)
+
   return (
     <>
       <Header />
       {children}
+      {isMedia800 && <MobileNavbar />}
       <div className='footer' />
     </>
   )
