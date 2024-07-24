560fa8c0548005324ffbd6681734b9e489a1c3f0
Richard Leshik <richardleshik@gmail.com>
2024-07-24 21:43:16 +0300

    feat: add CatalogMenu to MobileNavbar

--- app/components/modules/mobile-navbar/MobileNavbar.tsx
+++ app/components/modules/mobile-navbar/MobileNavbar.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { useLang } from '@/hooks'
+import { CatalogMenu } from '@/components'
 import {
   closeCatalogMenu,
   closeMenu,
@@ -26,25 +27,31 @@ export const MobileNavbar = () => {
   }
 
   return (
-    <div className='mobile-navbar'>
-      <Link href='/' className='mobile-navbar__btn'>
-        {translations[lang].breadcrumbs.main}
-      </Link>
-      <button
-        className='btn-reset mobile-navbar__btn'
-        onClick={handleOpenCatalogMenu}
-      >
-        {translations[lang].breadcrumbs.catalog}
-      </button>
-      <Link href='/favorites' className='btn-reset mobile-navbar__btn'>
-        {translations[lang].breadcrumbs.favorites}
-      </Link>
-      <Link href='/cart' className='btn-reset mobile-navbar__btn'>
-        {translations[lang].breadcrumbs.cart}
-      </Link>
-      <button className='btn-reset mobile-navbar__btn' onClick={handleOpenMenu}>
-        {translations[lang].common.more}
-      </button>
-    </div>
+    <>
+      <CatalogMenu />
+      <div className='mobile-navbar'>
+        <Link href='/' className='mobile-navbar__btn'>
+          {translations[lang].breadcrumbs.main}
+        </Link>
+        <button
+          className='btn-reset mobile-navbar__btn'
+          onClick={handleOpenCatalogMenu}
+        >
+          {translations[lang].breadcrumbs.catalog}
+        </button>
+        <Link href='/favorites' className='btn-reset mobile-navbar__btn'>
+          {translations[lang].breadcrumbs.favorites}
+        </Link>
+        <Link href='/cart' className='btn-reset mobile-navbar__btn'>
+          {translations[lang].breadcrumbs.cart}
+        </Link>
+        <button
+          className='btn-reset mobile-navbar__btn'
+          onClick={handleOpenMenu}
+        >
+          {translations[lang].common.more}
+        </button>
+      </div>
+    </>
   )
 }
