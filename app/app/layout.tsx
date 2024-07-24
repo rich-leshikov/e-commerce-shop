0e1f5825f4ce5055c9e219aab8f54524d7d2bafc
Richard Leshik <richardleshik@gmail.com>
2024-07-24 21:43:46 +0300

    style: add and connect catalog-menu.css

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Layout } from '@/components'
 
 import 'bootstrap/dist/css/bootstrap.css'
+import './global-styles/catalog-menu.css'
 import './global-styles/globals.css'
 import './global-styles/header.css'
 import './global-styles/menu.css'
