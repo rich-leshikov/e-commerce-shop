b7b364956bd32ddd6866c64f1462ad34c5796722
Richard Leshik <richardleshik@gmail.com>
2024-07-25 22:52:38 +0300

    feat: add CatalogMenuList component

---
+++ app/components/modules/header/CatalogMenuList.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { motion } from 'framer-motion'
+import Link from 'next/link'
+
+export const CatalogMenuList = ({ items }: { items: string[] }) => {
+  return (
+    <motion.ul
+      className='list-reset nav-menu__accordion'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {items.map((title, i) => (
+        <li
+          key={i}
+          className='nav-menu__accordion__item__list__item'
+          style={{ position: 'relative' }}
+        >
+          <Link
+            href='/catalog'
+            className='nav-menu__accordion__item__list__item__link'
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
+    </motion.ul>
+  )
+}
