908456c849ac0e681434e39fff7dbb2d7a351c97
Richard Leshik <richardleshik@gmail.com>
2024-07-27 20:59:56 +0300

    feat: add FooterLinks.tsx

---
+++ app/components/modules/footer/FooterLinks.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export const FooterLinks = () => (
+  <div className='footer__links'>
+    <span>
+      <a href='https://vc.ru'>vc.ru</a>
+    </span>
+    <span>
+      <a href='https://habr.com'>habr.com</a>
+    </span>
+  </div>
+)
