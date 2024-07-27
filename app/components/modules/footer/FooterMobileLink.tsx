2c27f81d48ae58ec1070aa01c5ec81b42cd80985
Richard Leshik <richardleshik@gmail.com>
2024-07-27 21:14:58 +0300

    feat: add FooterMobileLink.tsx

---
+++ app/components/modules/footer/FooterMobileLink.tsx
@@ -0,0 +1,8 @@
+'use client'
+import Link from 'next/link'
+
+export const FooterMobileLink = ({ text }: { text: string }) => (
+  <div className='footer__mobile'>
+    <Link href='#'>{text}</Link>
+  </div>
+)
