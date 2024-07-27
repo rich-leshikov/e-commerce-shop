0611b2389f7584a336f2975aa1b3d84e8a9f1cef
Richard Leshik <richardleshik@gmail.com>
2024-07-27 14:00:38 +0300

    feat: add SearchModal.tsx

---
+++ app/components/modules/header/SearchModal.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { useLang } from '@/hooks'
+
+export const SearchModal = () => {
+  const { lang, translations } = useLang()
+
+  return (
+    <div className='search-modal'>
+      <button className='btn-reset search-modal__close' />
+      <h3 className='search-modal__title'>
+        {translations[lang].header.search_products}
+      </h3>
+      <div className='search-modal__top'>
+        <label className='search-modal__label'>
+          <input type='text' className='search-modal__input' />
+          <span className='search-modal__floating_label'>
+            {translations[lang].header.search_infos}
+          </span>
+        </label>
+      </div>
+    </div>
+  )
+}
