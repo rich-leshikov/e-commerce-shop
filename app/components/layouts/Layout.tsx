cf70dc583f0d295a47ab3a4b74447f1c49f09754
Richard Leshik <richardleshik@gmail.com>
2024-07-27 14:37:08 +0300

    feat: add SearchModal to Layout.tsx

--- app/components/layouts/Layout.tsx
+++ app/components/layouts/Layout.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { FC, ReactNode } from 'react'
-import { Header, MobileNavbar } from '@/components'
+import { useUnit } from 'effector-react'
+import { AnimatePresence, motion } from 'framer-motion'
+import { Header, MobileNavbar, SearchModal } from '@/components'
 import { useMediaQuery } from '@/hooks'
+import { $searchModal } from '@/context'
 
 type Props = {
   children: ReactNode
@@ -9,12 +12,24 @@ type Props = {
 
 export const Layout: FC<Props> = ({ children }) => {
   const isMedia800 = useMediaQuery(800)
+  const searchModal = useUnit($searchModal)
 
   return (
     <>
       <Header />
       {children}
       {isMedia800 && <MobileNavbar />}
+      <AnimatePresence>
+        {searchModal && (
+          <motion.div
+            initial={{ opacity: 0, zIndex: 3 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <SearchModal />
+          </motion.div>
+        )}
+      </AnimatePresence>
       <div className='footer' />
     </>
   )
