bd32644ced7b7dda90fbd3c11421fac42674b4b5
Richard Leshik <richardleshik@gmail.com>
2024-07-28 22:28:27 +0300

    feat: add MainPage.tsx with Hero.tsx usage

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,9 @@
+import { MainPage } from '@/components/templates/main-page/MainPage'
+
 export default function Home() {
   return (
-    <main className={''}>
-      <h1>Page</h1>
-    </main>
+    <>
+      <MainPage />
+    </>
   )
 }
---
+++ app/components/templates/main-page/MainPage.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Hero } from '@/components'
+
+export const MainPage = () => {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
