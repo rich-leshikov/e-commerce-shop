65a9c6c1f0331ef43fdf77d904e89a6626ffd0fe
Richard Leshik <richardleshik@gmail.com>
2024-07-28 22:27:19 +0300

    feat: update HeroSlideTooltip.tsx

--- app/components/modules/main-page/hero/HeroSlideTooltip.tsx
+++ app/components/modules/main-page/hero/HeroSlideTooltip.tsx
@@ -4,18 +4,17 @@ import { IHeroSlideTooltip } from '@/types'
 
 import styles from '@/styles/main-page/index.module.scss'
 
-export const HeroSlideTooltip = ({ title, image }: IHeroSlideTooltip) => {
-  return (
-    <div className={`${styles.hero__slider__slide__popup} slide-popup`}>
-      <Image
-        className={styles.hero__slider__slide__popup__img}
-        src={image}
-        alt={title}
-      />
-      <p className={styles.hero__slider__slide__popup__inner}>
-        <b className={styles.hero__slider__slide__popup__title}>{title}</b>
-        <span className={styles.hero__slider__slide__popup__price}>$70</span>
-      </p>
-    </div>
-  )
-}
+export const HeroSlideTooltip = ({ title, image }: IHeroSlideTooltip) => (
+  <div className={`${styles.hero__slider__slide__popup} slide-popup`}>
+    <span className={styles.hero__slider__slide__popup__arrow} />
+    <Image
+      className={styles.hero__slider__slide__popup__img}
+      src={image}
+      alt={title}
+    />
+    <p className={styles.hero__slider__slide__popup__inner}>
+      <b className={styles.hero__slider__slide__popup__title}>{title}</b>
+      <span className={styles.hero__slider__slide__popup__price}>$70</span>
+    </p>
+  </div>
+)
